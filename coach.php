6dbf4133db048d920cb04b9465f55cb9c6c23c96
Guiri6G <58954712+Guiri6G@users.noreply.github.com>
2020-04-13 17:53:56 +0200

    update

--- coach.php
+++ coach.php
@@ -42,6 +42,7 @@
 
 	<!-- Modernizr JS -->
 	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script src="https://kit.fontawesome.com/dc82b1705e.js" crossorigin="anonymous"></script>
 	<!-- FOR IE9 below -->
 	<!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
@@ -62,8 +63,7 @@
 					<div class="container">
 						<div class="col-md-12 col-md-offset-0">
 							<div class="animate-box">
-								<h2>Contactez-nous</h2>
-								<p class="breadcrumbs"><span><a href="index.html">Home</a></span> <span>Contact</span></p>
+								<h2>COACH [PRENOM & NOM ]</h2>
 							</div>
 						</div>
 					</div>
@@ -71,66 +71,43 @@
 			</div>
 		</section>
 
-		<div id="colorlib-contact">
+		<div class="colorlib-about">
 			<div class="container">
+				<div class="row row-pb-lg">
+					<div class="col-md-6 animate-box">
+						<iframe width="560" height="315" src="https://www.youtube.com/embed/Y3L-9IlsVcI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+					<div class="col-md-6 animate-box">
+						<h2>Qui suis-je ? </h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis leo mi, scelerisque et vehicula nec, mattis eu lacus. Pellentesque pulvinar urna eget sapien laoreet dignissim. Fusce sollicitudin sodales massa, id facilisis lectus rutrum in. Nam sed enim sodales diam finibus volutpat id et nibh. Donec purus erat, volutpat sit amet hendrerit at, fermentum eget orci. In lacinia et metus id fringilla. In lacinia nibh quis quam posuere faucibus. Suspendisse non magna vitae era.</p>
+					</div>
+				</div>
 				<div class="row">
-					<div class="col-md-4 col-md-push-8 animate-box">
-					<h2>Herbalife </h2>
-						<div class="row">
-							<div class="col-md-12">
-								<div class="contact-info-wrap-flex">
-									<div class="con-info">
-										<p><span><i class="icon-location-2"></i></span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer aliquam ex et lectus gravida, vitae eleifend nisl eleifend. In at libero felis. Vivamus pretium finibus justo sed fermentum. Ut fringilla et massa id hendrerit. Vivamus congue dolor nec dolor cursus </p>
-									</div>
-			
-								</div>
-							</div>
-						</div>
+					<div class="col-md-14 col-md-offset-2 text-left colorlib-heading animate-box" style="margin-left: 2rem;">
+						<label>photo avant/après</label>&nbsp;&nbsp;
+						<img src="https://www.metrorollerdoors.com.au/wp-content/uploads/2018/02/unavailable-image.jpg" height="500px" width="310px"/>
+						<img src="https://www.metrorollerdoors.com.au/wp-content/uploads/2018/02/unavailable-image.jpg" height="500px" width="310px">
+						<label style="padding-left: 100px"> Toi aussi, tu peux y arriver !</label>
+						
 					</div>
-					<div class="col-md-8 col-md-pull-4 animate-box">
-						<h2></h2>
-						<form action="#">
-							<div class="row form-group">
-								<div class="col-md-6">
-									<!-- <label for="fname">First Name</label> -->
-									<input type="text" id="fname" class="form-control" placeholder="Prénom">
-								</div>
-								<div class="col-md-6">
-									<!-- <label for="lname">Last Name</label> -->
-									<input type="text" id="lname" class="form-control" placeholder="Nom">
-								</div>
-							</div>
+				</div>
+				<br><br>
+				<div class="row">
+					<div class="col-md-8 text-left animate-box">
+						<h2>Découvre mon quotidien</h2>
+						<br>
+						<a href="#" style="color: #7AC143;"><i class="fab fa-twitter fa-5x"></i> </a>
+						<a href="#" style="color: #7AC143; margin: 20px"><i class="fab fa-instagram fa-5x"></i> </a>
+						<a href="#" style="color: #7AC143;"><i class="fab fa-pinterest fa-5x"></i> </a>
 
-							<div class="row form-group">
-								<div class="col-md-12">
-									<!-- <label for="email">Email</label> -->
-									<input type="text" id="email" class="form-control" placeholder="E-mail">
-								</div>
-							</div>
 
-							<div class="row form-group">
-								<div class="col-md-12">
-									<!-- <label for="subject">Subject</label> -->
-									<input type="text" id="subject" class="form-control" placeholder="Votre problème">
-								</div>
-							</div>
 
-							<div class="row form-group">
-								<div class="col-md-12">
-									<!-- <label for="message">Message</label> -->
-									<textarea name="message" id="message" cols="30" rows="10" class="form-control" placeholder="Vote message"></textarea>
-								</div>
-							</div>
-							<div class="form-group">
-								<input type="submit" value="Envoyer votre message" class="btn btn-primary">
-							</div>
-						</form>		
+
 					</div>
+					
 				</div>
 			</div>
 		</div>
-
-		
 
 		<footer id="colorlib-footer">
 			<div class="container">
@@ -140,7 +117,7 @@
 						<p>Far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
 						<p>
 							<ul class="colorlib-social-icons">
-								<li><a href="#"><i class="icon-twitter"></i></a></li> <input type="text" name="">
+								<li><a href="#"><i class="icon-twitter"></i></a></li>
 								<li><a href="#"><i class="icon-facebook"></i></a></li>
 								<li><a href="#"><i class="icon-linkedin"></i></a></li>
 								<li><a href="#"><i class="icon-dribbble"></i></a></li>
@@ -208,7 +185,7 @@
 								 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --><br> 
-								Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a>, <a href="http://pexels.com/" target="_blank">Pexels</a>
+								 Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a>, <a href="http://pexels.com/" target="_blank">Pexels</a>
 							</p>
 						</div>
 					</div>
@@ -240,12 +217,16 @@ Copyright &copy;<script>document.write(new Date().getFullYear());</script> All r
 	<script src="js/magnific-popup-options.js"></script>
 	<!-- Counters -->
 	<script src="js/jquery.countTo.js"></script>
-	<!-- Google Map -->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
-	<script src="js/google_map.js"></script>
 	<!-- Main -->
 	<script src="js/main.js"></script>
 
 	</body>
 </html>
 
+
+<!-- <ul class="colorlib-social-icons">
+										<li><a href="#"><i class="icon-twitter"></i></a></li>
+										<li><a href="#"><i class="icon-facebook"></i></a></li>
+										<li><a href="#"><i class="icon-linkedin"></i></a></li>
+										<li><a href="#"><i class="icon-dribbble"></i></a></li>
+									</ul> -->
--- navbar.php
+++ navbar.php
@@ -1,3 +1,4 @@
+<script src="https://kit.fontawesome.com/dc82b1705e.js" crossorigin="anonymous"></script>
 <nav class="colorlib-nav" role="navigation">
 			<div class="top-menu">
 				<div class="container">
@@ -11,10 +12,43 @@
 								<li><a href="photo.php">Photo avant/après</a></li>
 								<li><a href="services.php">Opportunité professionnelle</a></li>
 								<li><a href="contact.php">Contactez-nous</a></li>
-								<li><a href="login.php">Compte challenger</a></li>
+								<li><a href="" data-toggle="modal" data-target="#modalLoginForm" ><i class="fas fa-user"></i> Se connecter</a></li>
 							</ul>
 						</div>
 					</div>
 				</div>
 			</div>
 		</nav>
+
+
+		<div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Connecte-toi !</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form method="POST" action="script/login.php">
+      <div class="modal-body mx-3">
+        <div class="md-form mb-5">
+          
+         <i class="fas fa-envelope prefix grey-text" style="float: left; margin-top: 15px;"></i> <center><input type="email" id="defaultForm-email" style="width: 80%" class="form-control validate" name="email" placeholder="E-mail"></center>
+        </div>
+        <br>
+        <div class="md-form mb-4">
+          <i class="fas fa-lock prefix grey-text" style="float: left; margin-top: 15px;" ></i>
+         <center> <input type="password" id="defaultForm-pass" style="width: 80%" class="form-control validate" name="password" placeholder="Mot de passe"> </center>
+        </div>
+
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button class="btn btn-success" type="submit"> Se connecter </button>
+      </div>
+  </form>
+    </div>
+  </div>
+</div>
+
