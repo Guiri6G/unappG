fd5216768f4d3d37546a34eac07a167cf7a81cd4
Guiri6G <58954712+Guiri6G@users.noreply.github.com>
2020-04-13 17:55:35 +0200

    Create login.php

---
+++ script/login.php
@@ -0,0 +1,47 @@
+<?php
+
+include "../connexion.php";
+
+$emailUtil = $_POST['email'];
+$password = $_POST['password'];
+
+
+try{
+
+	$req = "SELECT * FROM utilisateur WHERE emailUtil = '$emailUtil'";
+	$stmt = $pdo->prepare($req);
+	$stmt->execute();
+	$donnees = $stmt->fetch(PDO::FETCH_ASSOC);
+
+	if ($password == $donnees['mdpUtil']){
+
+
+		echo "yes";
+
+		$_SESSION['id'] = $donnees['id'];
+		$_SESSION['prenom'] = $donnees['prenomUtil'];
+		$_SESSION['rang'] = $donnees['rangUtil'];
+
+		$etatCo = 1;
+
+	} else {
+
+		echo "non";
+		echo "<script type='text/javascript'>alert('Veuillez correctement vérifier la validité des informations saisies.');</script>";
+
+
+		$etatCo = 0;
+	}
+
+
+}
+
+catch(Exception $e){
+		print "Erreur ! ".$e->getMessage(). "<br/>"; 
+	}
+
+header('location : ../index.php');
+exit();	
+
+
+?>
