fdaf6f34a19d9be36f278aed5e18a96a9127399b
ayoubynov <61156953+ayoubynov@users.noreply.github.com>
2020-04-13 19:03:47 +0200

    navbar.php

--- utilities/navbar.php
+++ utilities/navbar.php
@@ -8,7 +8,7 @@
 						</div>
 						<div class="col-md-10 text-right menu-1">
 							<ul>
-								<li class="active"><a href="../index.php">Accueil</a></li>
+								<li class="active"><a href="index.php">Accueil</a></li>
 								<li><a href="photo.php">Photo avant/après</a></li>
 								<li><a href="pro.php">Opportunité professionnelle</a></li>
 								<li><a href="contact.php">Contactez-nous</a></li>
