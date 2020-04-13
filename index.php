fd55987c67e96d473498076abc6e5aeb32328b86
ayoubynov <61156953+ayoubynov@users.noreply.github.com>
2020-04-13 18:33:40 +0200

    update index

--- index.php
+++ index.php
@@ -71,89 +71,14 @@
 				</div>
 			</div>
 		</section>
-
-		<!-- <div class="colorlib-featured">
-			<div class="row animate-box">
-				<div class="featured-wrap">
-					<div class="owl-carousel">
-						<div class="item">
-							<div class="col-md-8 col-md-offset-2">
-								<div class="featured-entry">
-									<img class="img-responsive" src="images/dashboard_full_1.jpg" alt="">
-								</div>
-							</div>
-						</div>
-						<div class="item">
-							<div class="col-md-8 col-md-offset-2">
-								<div class="featured-entry">
-									<img class="img-responsive" src="images/dashboard_full_2.jpg" alt="">
-								</div>
-							</div>
-						</div>
-						<div class="item">
-							<div class="col-md-8 col-md-offset-2">
-								<div class="featured-entry">
-									<img class="img-responsive" src="images/dashboard_full_3.jpg" alt="">
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="colorlib-services colorlib-bg-white">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4 text-center animate-box">
-						<div class="services">
-							<span class="icon">
-								<i class="icon-browser"></i>
-							</span>
-							<div class="desc">
-								<h3>Create your own template</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 text-center animate-box">
-						<div class="services">
-							<span class="icon">
-								<i class="icon-download"></i>
-							</span>
-							<div class="desc">
-								<h3>Automatic Backup Data</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-4 text-center animate-box">
-						<div class="services">
-							<span class="icon">
-								<i class="icon-layers"></i>
-							</span>
-							<div class="desc">
-								<h3>Page Builder</h3>
-								<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div> -->
-
 		<div class="colorlib-intro">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2 text-center colorlib-heading animate-box">
 						<h2>Qu'est-que c'est No Limit Center ? </h2>
+						<img src="images/logo.png" alt="" />
 						<p>Huic Arabia est conserta, ex alio latere Nabataeis contigua; opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.</div>
 				</div>
-				<!-- <div class="row">
-					<div class="col-md-12 animate-box">
-						<span class="play"><a href="https://vimeo.com/channels/staffpicks/93951774" class="pulse popup-vimeo"><i class="icon-play3"></i></a></span>
-					</div>
-				</div> -->
 			</div>
 		</div>
 
@@ -218,44 +143,7 @@
 				</div>
 			</div>
 		</div>
-
-		<div id="colorlib-counter" class="colorlib-counters" style="background-image: url(images/cover_img_1.jpg);" data-stellar-background-ratio="0.5">
-			<div class="overlay"></div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="col-md-4 col-sm-4 text-center animate-box">
-							<div class="counter-entry">
-								<span class="icon"><i class="flaticon-ribbon"></i></span>
-								<div class="desc">
-									<span class="colorlib-counter js-counter" data-from="0" data-to="1500" data-speed="5000" data-refresh-interval="50"></span>
-									<span class="colorlib-counter-label">Of customers are satisfied with our professional support</span>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-4 text-center animate-box">
-							<div class="counter-entry">
-								<span class="icon"><i class="flaticon-church"></i></span>
-								<div class="desc">
-									<span class="colorlib-counter js-counter" data-from="0" data-to="500" data-speed="5000" data-refresh-interval="50"></span>
-									<span class="colorlib-counter-label">Amazing preset options to be mixed and combined</span>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-4 text-center animate-box">
-							<div class="counter-entry">
-								<span class="icon"><i class="flaticon-dove"></i></span>
-								<div class="desc">
-									<span class="colorlib-counter js-counter" data-from="0" data-to="1200" data-speed="5000" data-refresh-interval="50"></span>
-									<span class="colorlib-counter-label">Average response time on live chat support channel</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	<?php include 'footer.php' ; ?>
+		<?php include 'footer.php' ; ?>
 	</div>
 
 	<div class="gototop js-top">
